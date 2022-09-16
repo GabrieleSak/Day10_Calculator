06a079192862e0dc29eb94aae60eb6bc0fa36bdc
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-16 12:42:38 +0300

    Add logo and fix bud (input numbers are float not int)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from art import logo
+
 def add(n1, n2):
     return n1 + n2
 
@@ -21,12 +23,26 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
 
-answer = operations[operation_symbol](num1, num2)
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = float(input("What's the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start new calculation.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
